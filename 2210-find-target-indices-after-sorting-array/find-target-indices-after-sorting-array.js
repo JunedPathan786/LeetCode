2784435597233d18e3aa3df08c44142745c29e86
Juned Pathan <156304962+JunedPathan786@users.noreply.github.com>
2025-03-21 17:11:09 +0530

    Time: 3 ms (34.53%) | Memory: 54.5 MB (33.73%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.js
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.js
@@ -4,24 +4,24 @@
  * @return {number[]}
  */
 var targetIndices = function(nums, target) {
-        let num = 0;
-        let tcount = 0;
+    let num = 0;
+    let tcount = 0;
 
-        for (let ele of nums) {
-            if (ele === target) {
-                tcount++;
-            } else if (ele < target) {
-                num++;
-            }
-        }
-
-        let ans = [];
-
-        while (tcount > 0) {
-            ans.push(num);
+    for(let ele of nums){
+        if(ele === target){
+            tcount++;
+        }else if(ele < target){
             num++;
-            tcount--;
         }
+    }
 
-        return ans;
+    let ans = [];
+
+    while(tcount > 0){
+        ans.push(num);
+        num++;
+        tcount--;
+    }   
+
+    return ans;
 };
