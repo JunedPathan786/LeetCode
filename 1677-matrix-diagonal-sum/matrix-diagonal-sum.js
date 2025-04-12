5695d99ae4e82498c931aba8a60f956184b908ca
Juned Pathan <156304962+JunedPathan786@users.noreply.github.com>
2025-04-12 12:16:16 +0530

    Time: 1 ms (17.46%) | Memory: 56 MB (5.63%) - LeetSync

---
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var diagonalSum = function(mat) {
+    let sum = 0;
+    
+    let n = mat.length;
+
+    for(let i=0; i<n; i++){
+        for(let j=0; j<n; j++){
+
+            if(i==j || i+j == n-1){
+                sum += Math.floor(mat[i][j])
+            }  
+        }
+    }
+
+    return sum;
+};
