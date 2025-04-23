0ee5664a3be93372a260db1a68d11081cb9de137
Juned Pathan <156304962+JunedPathan786@users.noreply.github.com>
2025-04-23 17:42:40 +0530

    Time: 49 ms (64.05%) | Memory: 58.3 MB (13.34%) - LeetSync

---
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.js
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+    node.val = node.next.val;     // Step 1: Copy value from next node
+    node.next = node.next.next;   // Step 2: Bypass the next node
+};
