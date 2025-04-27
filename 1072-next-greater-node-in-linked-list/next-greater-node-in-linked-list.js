67ac9e23980d35569466496c7748d678c7f18652
Juned Pathan <156304962+JunedPathan786@users.noreply.github.com>
2025-04-27 12:13:50 +0530

    Time: 7 ms (82.68%) | Memory: 63.7 MB (47.24%) - LeetSync

--- 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.js
+++ 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.js
@@ -5,30 +5,67 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
+
 /**
  * @param {ListNode} head
  * @return {number[]}
  */
-function nextLargerNodes(head) {
-    let values = [];
-    let current = head;
-    
-    // Convert linked list to array
-    while (current) {
-        values.push(current.val);
-        current = current.next;
-    }
-    
-    let answer = new Array(values.length).fill(0);
-    let stack = []; // Stack to keep indices
 
-    for (let i = 0; i < values.length; i++) {
-        while (stack.length > 0 && values[i] > values[stack[stack.length - 1]]) {
-            let idx = stack.pop();
-            answer[idx] = values[i];
-        }
-        stack.push(i);
+function sizeLL(head){
+    let curr = head;
+    let count = 0;
+
+    while(curr != null){
+        count++;
+        curr = curr.next;
     }
-    
-    return answer;
+    return count;
+}
+
+function reverseLL(head){
+    let curr = head;
+    let pre = null;
+
+    while(curr != null){
+        let temp = curr.next;
+        curr.next = pre;
+        pre = curr;
+        curr = temp;
+    }
+    return pre;
+}
+
+function nextLargerNodes(head) {
+    let size = sizeLL(head);
+    let arr = new Array(size).fill(0);
+
+    let nHead = reverseLL(head);
+
+    let stack = [];
+    stack.push(nHead.val);
+
+    let curr = nHead.next;
+    let ptr = size - 2;
+
+    while (ptr >= 0 && curr !== null) {
+        let ele = curr.val;
+        curr = curr.next;
+
+        while (stack.length > 0 && stack[stack.length - 1] <= ele) {
+            stack.pop();
+        }
+
+        if (stack.length === 0) {
+            arr[ptr] = 0;
+        } else {
+            arr[ptr] = stack[stack.length - 1];
+        }
+
+        stack.push(ele);
+        ptr--;
+    }
+
+    arr[size - 1] = 0; // Last node will always have 0 as there is no next node
+
+    return arr;
 }
