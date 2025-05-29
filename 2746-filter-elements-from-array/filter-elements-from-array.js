17586de7c44d9b8938ceea259b8d45781895d280
Juned Pathan <156304962+JunedPathan786@users.noreply.github.com>
2025-05-29 21:56:29 +0530

    Time: 41 ms (61.73%) | Memory: 52.9 MB (90.33%) - LeetSync

--- 2746-filter-elements-from-array/filter-elements-from-array.js
+++ 2746-filter-elements-from-array/filter-elements-from-array.js
@@ -7,9 +7,7 @@ var filter = function(arr, fn) {
 
     const tempArr = []
     for(let i=0; i<arr.length; i++){
-        if(fn(arr[i], i)){
-            tempArr.push(arr[i])
-        }
+        if(fn(arr[i], i))tempArr.push(arr[i])
     }
     return tempArr
 };
