e0a4ac2fb8ff42ef82dd57f2a7affba530b36d8f
Juned Pathan <156304962+JunedPathan786@users.noreply.github.com>
2026-02-16 16:40:49 +0530

    Time: 21 ms (19.99%) | Memory: 63.2 MB (22.62%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -4,7 +4,12 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-        // Step 1: Count the frequency of each element
+
+    if(k == nums.length){
+        return nums
+    }
+    
+    // Step 1: Count the frequency of each element
     const countMap = new Map();
     for (const num of nums) {
         countMap.set(num, (countMap.get(num) || 0) + 1);
@@ -13,6 +18,7 @@ var topKFrequent = function(nums, k) {
     // Step 2: Create "buckets" (an array of arrays), where the index is the frequency
     // The maximum possible frequency is the length of the input array, n.
     const buckets = new Array(nums.length + 1).fill(null).map(() => []);
+
     for (const [num, count] of countMap.entries()) {
         buckets[count].push(num);
     }
@@ -20,6 +26,7 @@ var topKFrequent = function(nums, k) {
     // Step 3: Iterate through the buckets from highest frequency down to 1
     // and collect elements until k elements are found.
     const result = [];
+
     for (let i = buckets.length - 1; i >= 1; i--) {
         for (const num of buckets[i]) {
             result.push(num);
