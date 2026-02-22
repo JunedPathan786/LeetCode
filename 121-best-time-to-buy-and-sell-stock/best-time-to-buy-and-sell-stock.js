550ae6d03886ed79ff35282c2da1483ccbc0bb3c
Juned Pathan <156304962+JunedPathan786@users.noreply.github.com>
2026-02-22 10:18:20 +0530

    Time: 3 ms (60.25%) | Memory: 64.6 MB (90.89%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,21 +3,16 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let n = prices.length;
-    let maxVal = [];
+    let min = prices[0]
+    let profit = 0
 
-    maxVal[n - 1] = prices[n - 1];
+    for(let i=0; i < prices.length; i++){
+        if(prices[i] < min){
+            min = prices[i]
+        }
 
-    for (let i = n - 2; i >= 0; i--) {
-        maxVal[i] = Math.max(maxVal[i + 1], prices[i]);
+        profit = Math.max(profit, prices[i]-min)
     }
 
-    let ans = 0;
-
-    for (let i = 0; i < prices.length; i++) {
-        let currPro = maxVal[i] - prices[i];
-        ans = Math.max(ans, currPro);
-    }
-
-    return ans;
+    return profit
 };
