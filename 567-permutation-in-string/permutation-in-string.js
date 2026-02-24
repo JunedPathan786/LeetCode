1a59ea3735ddde469f26bb48251bb9197cd61d69
Juned Pathan <156304962+JunedPathan786@users.noreply.github.com>
2026-02-24 14:59:44 +0530

    Time: 7 ms (94.35%) | Memory: 54.8 MB (97.71%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function(s1, s2) {
+    if(s1.length  > s2.length){
+        return false
+    }
+
+    let s1Map = new Array(26).fill(0)
+    let s2Map = new Array(26).fill(0)
+
+    for(let i =0 ; i < s1.length ; i++){
+        s1Map[s1.charCodeAt(i) - 97]++
+        s2Map[s2.charCodeAt(i) - 97]++
+    }
+
+    if(matches(s1Map, s2Map)){
+        return true
+    }
+
+    function matches(map1, map2){
+        for(let i =0 ; i <26; i++){
+            if(map1[i] !== map2[i]){
+            return false
+            }
+        }
+        return true
+    }
+
+    for(let i = s1.length ; i < s2.length; i++){
+        s2Map[s2.charCodeAt(i) - 97]++
+
+        s2Map[s2.charCodeAt(i - s1.length) - 97]--
+
+        if(matches(s1Map, s2Map)) return true
+    }
+
+    return false
+};
