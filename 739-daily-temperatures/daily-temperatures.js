0fa4cae57edb338461eb23c6f9e09d3b42e14779
Juned Pathan <156304962+JunedPathan786@users.noreply.github.com>
2026-03-02 21:56:14 +0530

    Time: 15 ms (76.46%) | Memory: 79.3 MB (51.86%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures) {
+    let n = temperatures.length
+    let answer = new Array(n).fill(0)
+    let stack = []
+
+    for(let i=0; i<n; i++){
+        while(stack.length && temperatures[i] > temperatures[stack[stack.length -1]]){
+            let index = stack.pop()
+            answer[index] = i - index
+        }
+        stack.push(i)
+    }
+    return answer
+};
