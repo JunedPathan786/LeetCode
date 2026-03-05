498fecbb13477a72b0b144d5447416024295e261
Juned Pathan <156304962+JunedPathan786@users.noreply.github.com>
2026-03-05 10:17:26 +0530

    Time: 0 ms (100.00%) | Memory: 57.9 MB (74.01%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -4,19 +4,18 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let start = 0;
-    let end = nums.length-1;
+    let left = 0, right = nums.length - 1
 
-    while(start<=end){
-        let mid = Math.floor((start + end) / 2);
+    while(left <= right){
+        let mid = Math.floor(left + (right - left) / 2)
 
-        if(nums[mid] === target){
-            return mid;
-        }else if(target > nums[mid]){
-            start = mid + 1;
+        if(nums[mid] == target){
+            return mid
+        }else if(nums[mid] < target){
+            left = mid + 1
         }else{
-            end = mid - 1;
+            right = mid - 1
         }
     }
-    return -1;
+    return -1
 };
