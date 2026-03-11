e10d873651752d6344ad34243aa742961d682336
Juned Pathan <156304962+JunedPathan786@users.noreply.github.com>
2026-03-11 10:40:40 +0530

    Time: 1 ms (99.07%) | Memory: 58 MB (85.83%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -3,25 +3,40 @@
  * @param {number[]} nums2
  * @return {number}
  */
-var findMedianSortedArrays = function (nums1, nums2) {
-    const m = nums1.length;
-    const n = nums2.length;
-    const f = (i, j, k) => {
-        if (i >= m) {
-            return nums2[j + k - 1];
+var findMedianSortedArrays = function(nums1, nums2) {
+   if(nums1.length > nums2.length){
+            return findMedianSortedArrays(nums2, nums1)
         }
-        if (j >= n) {
-            return nums1[i + k - 1];
+
+        let m = nums1.length
+        let n = nums2.length
+
+        let left = 0
+        let right = m
+
+        let half = Math.floor((m+n+1)/2)
+
+        while(left <= right){
+            let i = Math.floor((left+right)/2)
+            let j = half - i
+
+            let leftA = (i === 0) ? -Infinity : nums1[i - 1]
+            let rightA = (i === m) ? Infinity : nums1[i]
+            
+            let leftB = (j === 0) ? -Infinity : nums2[j - 1]
+            let rightB = (j === n) ? Infinity : nums2[j]
+
+            if(leftA <= rightB && leftB <= rightA){
+
+                if((m+n)%2 === 0){
+                    return (Math.max(leftA, leftB) + Math.min(rightA, rightB)) / 2
+                }else{
+                    return Math.max(leftA, leftB)
+                }
+            }else if(leftA > rightB){
+                right = i - 1
+            }else{
+                left = i + 1
+            }
         }
-        if (k == 1) {
-            return Math.min(nums1[i], nums2[j]);
-        }
-        const p = Math.floor(k / 2);
-        const x = i + p - 1 < m ? nums1[i + p - 1] : 1 << 30;
-        const y = j + p - 1 < n ? nums2[j + p - 1] : 1 << 30;
-        return x < y ? f(i + p, j, k - p) : f(i, j + p, k - p);
-    };
-    const a = f(0, 0, Math.floor((m + n + 1) / 2));
-    const b = f(0, 0, Math.floor((m + n + 2) / 2));
-    return (a + b) / 2;
 };
