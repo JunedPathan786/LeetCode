857f91ca172a86e359fd300e91f06abd53f13ad5
Juned Pathan <156304962+JunedPathan786@users.noreply.github.com>
2026-03-13 10:27:20 +0530

    Time: 41 ms (97.54%) | Memory: 58.4 MB (82.08%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.js
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -11,21 +11,22 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    if(head == null || head.next == null){
-        return false;
-    }
-
-    let fast = head;
-    let slow = head;
-
-    while(fast != null && fast.next != null){
-         slow = slow.next;
-        fast = fast.next.next;
-
-        if(fast == slow){
-            return true;
+    if(head == null){
+            return false
         }
-    }
 
-    return false;
+        let slow = head
+        let fast = head
+
+        while(fast != null && fast.next != null){
+
+            slow = slow.next
+            fast = fast.next.next
+
+            if(slow === fast){
+                return true
+            }
+        }
+
+        return false
 };
